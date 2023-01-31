fa2f0a736d0557c395b05e6029fe56601529e1a2
Miranda84315 <miranda84315@gmail.com>
2023-01-31 23:54:54 +0800

    LeetCode: 20230103_Easy_95

---
+++ 95. Unique Binary Search Trees II.c
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+
+void traversal(struct TreeNode* root, int* arr, int* size)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    
+    traversal(root->left, arr, size);
+    arr[(*size)++] = root->val;
+    traversal(root->right, arr, size);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize){
+
+    int *ret = malloc(sizeof(int) * 100);
+    *returnSize = 0;
+
+    if (root != NULL)
+    {
+        traversal(root, ret, returnSize);
+    }
+
+    return ret;
+}
