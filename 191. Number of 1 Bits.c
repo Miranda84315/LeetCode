5d1c8f8b3ed127f4e1bb25162b39ebc12c987a41
miranda84315 <miranda84315@gmail.com>
2023-02-21 22:56:43 +0800

    LeetCode: 20230221_Easy_190_191_2119

---
+++ 191. Number of 1 Bits.c
@@ -0,0 +1,15 @@
+int hammingWeight(uint32_t n) {
+    int ret = 0;
+
+    for (int i = 0; i < 32; i++)
+    {
+        if (n & 0x1)
+        {
+            ret++;
+        }
+
+        n = n >> 1;
+    }
+
+    return ret;
+}
